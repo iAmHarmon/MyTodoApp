ad82c62340e9e0b4e3e2a0228692f5934bd4a532
Harmon <harmonh@gmail.com>
2022-08-28 22:41:17 -0700

    Pushed auth to AWS

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,21 @@
-export type AmplifyDependentResourcesAttributes = {};
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "mytodoapp5d96bd5fPostConfirmation": {
+            "Name": "string",
+            "Arn": "string",
+            "LambdaExecutionRole": "string",
+            "Region": "string"
+        }
+    },
+    "auth": {
+        "mytodoapp5d96bd5f": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
