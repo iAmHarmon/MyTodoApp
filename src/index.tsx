f502065f31f519e1e18111469b9977f1b5b1cca5
Harmon <harmonh@gmail.com>
2022-10-04 11:29:02 -0700

    Initial install and react navigation config

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
 import { store } from './store/store';
 import App from './App';
 
 export default () => {
   return (
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <NavigationContainer>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </NavigationContainer>
   );
 };
